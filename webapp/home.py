a5948547deca8f6b224e7d9ad0769cff4061cd47
Selim Can Polat <selimcanpolat1@gmail.com>
2022-02-08 11:05:05 +0300

    Created navigation menu for Home

--- webapp/home.py
+++ webapp/home.py
@@ -4,9 +4,35 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        main_div = jp.Div(a=wp, classes="bg-gray-800 "  # main div for background (like a canvas)
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left",
+                            bordered=True)
+
+
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
+
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+
+        container = jp.QPageContainer(a=layout)
+
+        main_div = jp.Div(a=container, classes="bg-gray-800 "  # main div for background (like a canvas)
                                         "h-screen")  # covers the entire screen
         jp.Div(a=main_div,
                text="This is the Home page!",
@@ -23,3 +49,10 @@ class Home:
                 """, classes="text-lg")
 
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
